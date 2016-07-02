790a9eb84fb9858083edecc65349f86bfdf9901c
Jake Faulkner <jakefaulkn@gmail.com>
2016-07-02 17:43:21 +1200

    First working build

--- src/main.scm
+++ src/main.scm
@@ -1,6 +1,6 @@
 ;; The main scheme file, for all the cool command line stuff
-(declare (use jselect))
-(declare (use httpapis))
+(declare (uses jselect))
+(declare (uses httpapis))
 (require-extension srfi-37)
 (use srfi-37)
 (define options
@@ -13,5 +13,5 @@
 -s --search S Search itunes for S
 ")))))
 (define (run args)
-  (print (jselect '(trackName) (itunes-query (car args)))))
+  (print (jselect '(trackName) (itunes-results (itunes-query (car args))))))
 (run (cdr (argv)))
