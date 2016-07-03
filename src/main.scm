76b924b5bd56cd296ca73acfa4ff016c5e1f4aef
Jake Faulkner <jakefaulkn@gmail.com>
2016-07-03 23:45:52 +1200

    Improve UX for command line

--- src/main.scm
+++ src/main.scm
@@ -1,17 +1,50 @@
 ;; The main scheme file, for all the cool command line stuff
 (declare (uses jselect))
 (declare (uses httpapis))
+(require-extension fmt-unicode)
+(require-extension fmt)
 (require-extension srfi-37)
 (use srfi-37)
-(define options
-  '(;; help
-    (option
-     '(#\h "help") #f #f
-     (lambda _
-       (print "Usage musicfinder [option] ARG ...
+(use fmt)
+(use fmt-unicode)
+(define help
+ (option
+  '(#\h "help") #f #f
+  (lambda _
+    (print "Usage musicfinder [option] ARG ...
 -h --help     show this text
--s --search S Search itunes for S
-")))))
-(define (run args)
-  (print (jselect '(trackName) (itunes-results (itunes-query (car args))))))
-(run (cdr (argv)))
+-s --search S Search itunes for S (S follows pattern artist - album - title)
+")
+    (exit))))
+(define (print-table rows)
+  ;; (fmt-let 'string-width unicode-string-width (apply-cat (fmt #t (dsp "test"))))
+  (let ((rows (cons '((artistName . "Artist") (collectionName . "Album") (trackName . "Title")) rows)))
+    (fmt #t (fmt-unicode (tabular
+                          "| "
+                          (dsp (string-join (map (lambda (r) (cdr (assoc 'artistName r))) rows) "\n"))
+                          " | "
+                          (dsp (string-join (map (lambda (r) (cdr (assoc 'collectionName r))) rows) "\n"))
+                          " | "
+                          (dsp (string-join (map (lambda (r) (cdr (assoc 'trackName r))) rows) "\n"))
+                          " |")))))
+(define search
+  (option
+   '(#\s "search") #t #f
+   (lambda (option option-char query vals)
+     (print-table (jselect '(artistName collectionName trackName) (itunes-results (itunes-query query))))
+     vals)))
+
+(define (run)
+  (for-each
+   (lambda (x) (print* x #\space))
+   (reverse
+    (args-fold
+     (command-line-arguments)
+     (list help search)
+     (lambda (o n x vals)
+       (error "unrecognized option" n) )
+     cons
+     '())))
+  ;; (jselect '(trackName) (itunes-results (itunes-query (car args))))
+  )
+(run)
